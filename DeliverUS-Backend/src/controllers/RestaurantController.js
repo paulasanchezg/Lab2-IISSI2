45d0c0b719b47b94f1a318f67d3e0bdaf946ea3f
Paula <pausangom1@alum.us.es>
2024-03-04 21:34:23 +0100

    lab 2 clase

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -2,10 +2,12 @@ import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../mod
 
 const index = async function (req, res) {
   try {
+    // 1. Try, catch
+    // findAll -> busca todos los restaurantes de la base de datos
     const restaurants = await Restaurant.findAll(
       {
         attributes: { exclude: ['userId'] },
-        include:
+        include: // incluye información de las relaciones del modelo (Restaurant)
       {
         model: RestaurantCategory,
         as: 'restaurantCategory'
@@ -22,19 +24,62 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  // newRestaurant.userId = req.user.id // authenticated user
+  newRestaurant.userId = 1
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + req.params.restaurantId
+    } else {
+      message = 'Could not delete restaurant.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,17 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
+  // No podemos asignar un verbo http para dos funciones de controladores
+  // FR2: leer restaurante para owner, el propietario de un restaurante solo podrá leer su restaurante y en un futuro añadiremos una ruta y un controlador para ello
+  app.route('/restaurants') // path
+    .get( // lista de restaurantes que ve el cliente
+      RestaurantController.index)
+    .post(RestaurantController.create)
 
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+    .put(RestaurantController.update)
+    .delete(RestaurantController.destroy)
 }
+
 export default loadFileRoutes
